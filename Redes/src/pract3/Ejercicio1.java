7b75eb982ec4a1b1a6d0c52c105843915fefdb6e
Cotel <miguelcoletomunoz@hotmail.com>
2015-10-30 19:41:33 +0100

    Replaced deprecated methods and finished.

--- Redes/src/pract3/Ejercicio1.java
+++ Redes/src/pract3/Ejercicio1.java
@@ -1,8 +1,9 @@
 package pract3;
 
-import java.io.DataInputStream;
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
  
 public class Ejercicio1 {
@@ -19,13 +20,13 @@ public class Ejercicio1 {
     }
  
     public static void ejercicio3() {
-        Socket s;
-        DataInputStream dis;
+        Socket s;        
+        BufferedReader br; 
         try {
             s = new Socket("smtp.upv.es", 25);
-            dis = new DataInputStream(s.getInputStream());
-            System.out.println(dis.readLine());
-            dis.close();
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            System.out.println(br.readLine());
+            br.close();
             s.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -35,12 +36,12 @@ public class Ejercicio1 {
  
     public static void ejercicio4() {
         Socket s;
-        DataInputStream dis;
+        BufferedReader br;
         try {
             s = new Socket("ntp.upv.es", 13);
-            dis = new DataInputStream(s.getInputStream());
-            System.out.println(dis.readLine());
-            dis.close();
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            System.out.println(br.readLine());
+            br.close();
             s.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -49,19 +50,19 @@ public class Ejercicio1 {
  
     public static void ejercicio5() {
         Socket s;
-        DataInputStream dis;
+        BufferedReader br;
         DataOutputStream dos;
         String res;
         try {
             s = new Socket("www.upv.es", 80);
-            dis = new DataInputStream(s.getInputStream());
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
             dos = new DataOutputStream(s.getOutputStream());
-            dos.writeUTF("GET / HTTP/1.0\r\n\r\n");
+            dos.writeBytes("GET / HTTP/1.0\r\n\r\n");
             do {
-                res = dis.readLine();
+                res = br.readLine();
                 System.out.println(res);
             } while (res != null);
-            dis.close();
+            br.close();
             dos.close();
             s.close();
         } catch (IOException e) {
@@ -69,7 +70,7 @@ public class Ejercicio1 {
         }
     }
  
-    public void ejercicio6() {
+    public static void ejercicio6() {
     	Socket s;
     	try {
 			s = new Socket("smtp.upv.es", 25);
@@ -93,6 +94,7 @@ public class Ejercicio1 {
         ejercicio3();
         ejercicio4();
         ejercicio5();
+        ejercicio6();
  
     }
  
